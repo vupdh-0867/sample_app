e7b8129dc6c52e30fd5d0620b6288c6ef1649cf1
vupdh-0867 <phan.dang.hai.vu@sun-asterisk.com>
2019-03-22 10:11:44 +0700

    Chapter 11: Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      remember_or_forget user
-      redirect_back_or user
+      handle_user_is_activated_or_not user
     else
       flash.now[:danger] =
         t "controllers.sessions_controller.login_fail_message"
       render :new
+    end
+  end
+
+  def handle_user_is_activated_or_not user
+    if user.activated?
+      log_in user
+      remember_or_forget user
+      redirect_back_or user
+    else
+      flash[:warning] = t "controllers.sessions_controller.unactivated_account"
+      redirect_to root_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save{email.downcase!}
   has_secure_password
   validates :email, presence: true,
     length: {maximum: Settings.app.models.user.email_max_length},
@@ -13,10 +14,10 @@ class User < ApplicationRecord
     allow_nil: true
 
   def self.digest string
-    BCrypt::Password.create(string, cost: get_cost)
+    BCrypt::Password.create(string, cost: User.get_cost)
   end
 
-  def get_cost
+  def self.get_cost
     return BCrypt::Engine.min_cost if ActiveModel::SecurePassword.min_cost
     BCrypt::Engine.cost
   end
@@ -30,12 +31,33 @@ class User < ApplicationRecord
     update remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update remember_digest: nil
+  end
+
+  def activate
+    update_attributes :activated, true
+    update_attributes :activated_at, Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
